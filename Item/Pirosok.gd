300417fea5a8ea5a4a73cfea17e65cbe7632f44a
Pavel <ziengri@gmail.com>
2023-04-03 15:43:02 +0300

    kitchen v3

---
+++ Item/Pirosok.gd
@@ -0,0 +1,11 @@
+extends Item
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func use_effect():
+	print("kinul piroshok")
+#	self.queue_free()
